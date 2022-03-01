034b0295e020e42b2b8e5471f7d506682da08db8
noviceprogrammer2 <simonsbrian91@gmail.com>
2022-02-28 21:50:09 -0500

    Added Coin Flip Program that calculates chances of getting 6 heads or tails in a row over 100 flips repeated 10000 times

--- Chapter 4 Lists/Coin Flip Streaks.py
+++ Chapter 4 Lists/Coin Flip Streaks.py
@@ -2,7 +2,7 @@
 
 import random
 numberOfStreaks = 0
-
+streak = 0
 for experimentNumber in range(10000):
     # Code that creates a list of 100 'heads' or 'tails' values.
     coinflips = []
@@ -14,12 +14,12 @@ for experimentNumber in range(10000):
 
     # Code that checks if there is a streak of 6 heads or tails in a row.'
 
-    for i in range(len(coinflips)):
-        if coinflips[i] == coinflips[i-1]:
-            streak += 1
+    for i in range(len(coinflips)): #for counter in range of the length of coinflips (100)
+        if coinflips[i] == coinflips[i-1]: #if the number at that spot is equal to the number before it in the list, streak +1
+            streak = streak +  1
         else:
-            streak = 0
-        if streak == 6:
+            streak = 0 #no repeat sets streak back to 0
+        if streak == 6: #if streak equals 6, than number of streaks += 1
             numberOfStreaks=numberOfStreaks+1
 
 print('Chance of streak: %s%%' % (numberOfStreaks / (10000)))
