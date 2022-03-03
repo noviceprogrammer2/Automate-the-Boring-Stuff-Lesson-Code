5129abee704eaa5a1e8214bc22dbdb79564dbd8d
noviceprogrammer2 <simonsbrian91@gmail.com>
2022-03-02 19:12:09 -0500

    Chess Dictionary can count total pieces accurately, need to get conditions in place

--- Chapter 5 Dictionaries & Structuring Data/Chess Dictionary Validator.py
+++ Chapter 5 Dictionaries & Structuring Data/Chess Dictionary Validator.py
@@ -12,66 +12,36 @@
 #then create argument to check
 
 def isValidChessBoard(board):
-    # creating keys to map valid spaces
-    validspaces = {'1a':'', '1b': '','1c':'', '1d':'', '1e':'', '1f':'', '1g':'', '1h':'',
-                    '2a':'', '2b':'', '2c':'', '2d':'', '2e':'', '2f':'', '2g':'', '2h':'',
-                    '3a':'', '3b':'', '3c':'', '3d':'', '3e':'', '3f':'', '3g':'', '3h':'',
-                    '4a':'', '4b':'', '4c':'', '4d':'', '4e':'', '4f':'', '4g':'', '4h':'',
-                    '5a':'', '5b':'', '5c':'', '5d':'', '5e':'', '5f':'', '5g':'', '5h':'',
-                    '6a':'', '6b':'', '6c':'', '6d':'', '6e':'', '6f':'', '6g':'', '6h':'',
-                    '7a':'', '7b':'', '7c':'', '7d':'', '7e':'', '7f':'', '7g':'', '7h':'',
-                    '8a':'', '8b':'', '8c':'', '8d':'', '8e':'', '8f':'', '8g':'', '8h':''}
-    numberofbKings = 0
-    numberofbQueens = 0
-    numberofbPawns = 0
-    numberofwKings = 0
-    numberofwQueens = 0
-    numberofwPawns = 0
-    totalwpieces = 0
-    totalbpieces = 0
-
+    #counters
+    numberofKings = 0
+    numberofPawns=0
+    totalpieces=0
+    validspaces = {'1a':'', '1b': '','1c':'', '1d':'', '1e':'', '1f':'', '1g':'', '1h':'', '2a':'', '2b':'', '2c':'', '2d':'', '2e':'', '2f':'', '2g':'', '2h':'','3a':'', '3b':'', '3c':'', '3d':'', '3e':'', '3f':'', '3g':'', '3h':'','4a':'', '4b':'', '4c':'', '4d':'', '4e':'', '4f':'', '4g':'', '4h':'','5a':'', '5b':'', '5c':'', '5d':'', '5e':'', '5f':'', '5g':'', '5h':'','6a':'', '6b':'', '6c':'', '6d':'', '6e':'', '6f':'', '6g':'', '6h':'','7a':'', '7b':'', '7c':'', '7d':'', '7e':'', '7f':'', '7g':'', '7h':'','8a':'', '8b':'', '8c':'', '8d':'', '8e':'', '8f':'', '8g':'', '8h':''}
     #checks if there's at least a king or queen in board
-    if 'bking' not in board.values:
+    if 'bking' in board.values() == False:
         return False
-    if 'bqueen' not in board.values:
-        return False
-    if 'wking' not in board.values:
-        return False
-    if 'wqueen' not in board.values:
+    if 'wking' in board.values() == False:
         return False
     # if board key is not in valid spaces, returns false
-    if board.keys not in validspaces
-        return False
+    for key in board.keys():
+        if key not in validspaces.keys():
+            print('Value: %s not in validspace on chess board'%(key))
+            return False
 
-    for board.keys in validspaces: #for board.keys in validspaces (if the board key happens to be a valid space)
-        #Black piece checker
-        if 'bking' in board.values:
-            numberofbKings = numberofbKings + 1
-            if numberofbKings > 1:
-                return False
-        if'bqueen' in board.values:
-            numberofbQueens = numberofbQueens + 1
-            if numberofbQueens > 1:
-                return False
-        if 'bpawn' in board.values:
-            numberofbPawns = numberofbPawns + 1
-            if numberofbPawns > 16
-                return False
-        #white piece checker
-            if 'wking' in board.values:
-                numberofwKings = numberofwKings +1
-                if numberofwKings > 1:
-                    return False
-            if 'wqueen' in board.values:
-                numberofwQueens = numberofwQueens + 1
-                if numberofwQueens > 1:
-                    return False
+    for v in board.values(): #checks each value and makes assignments
+        #king checker
+        if 'bking' or 'wking' in board.values():
+            numberofKings = numberofKings + 1
+            totalpieces = totalpieces + 1
+        #pawn checker
+        elif 'wrook' or 'brook' or 'wbishop' or 'bbishop' or 'wknight' or 'bknight' in board.values:
+            totalpieces = totalpieces + 1
+        elif 'wPawn' or 'bPawn' in board.values():
+            numberofPawns = numberofPawns + 1
+            totalpieces = totalpieces + 1
+    print(totalpieces)
 
 
-
-    return False
-
-
-
-board = {'1h': 'bking', '6c': 'wqueen', '2g': 'bbishop', '5h': 'bqueen', '3e': 'wking'}
+testboard = {'1h': 'bking', '6c': 'wqueen', '2g': 'bbishop', '5h': 'bqueen', '3e': 'wking'}
+isValidChessBoard(testboard)
 
