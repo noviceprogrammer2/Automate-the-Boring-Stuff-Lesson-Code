3668d8b67762c535c64e746c9c1eba03e96394c2
noviceprogrammer2 <simonsbrian91@gmail.com>
2022-03-02 19:18:20 -0500

    Chess Dictionary can now accurately handle situations where no kings, or valid spaces are present

--- Chapter 5 Dictionaries & Structuring Data/Chess Dictionary Validator.py
+++ Chapter 5 Dictionaries & Structuring Data/Chess Dictionary Validator.py
@@ -18,10 +18,10 @@ def isValidChessBoard(board):
     totalpieces=0
     validspaces = {'1a':'', '1b': '','1c':'', '1d':'', '1e':'', '1f':'', '1g':'', '1h':'', '2a':'', '2b':'', '2c':'', '2d':'', '2e':'', '2f':'', '2g':'', '2h':'','3a':'', '3b':'', '3c':'', '3d':'', '3e':'', '3f':'', '3g':'', '3h':'','4a':'', '4b':'', '4c':'', '4d':'', '4e':'', '4f':'', '4g':'', '4h':'','5a':'', '5b':'', '5c':'', '5d':'', '5e':'', '5f':'', '5g':'', '5h':'','6a':'', '6b':'', '6c':'', '6d':'', '6e':'', '6f':'', '6g':'', '6h':'','7a':'', '7b':'', '7c':'', '7d':'', '7e':'', '7f':'', '7g':'', '7h':'','8a':'', '8b':'', '8c':'', '8d':'', '8e':'', '8f':'', '8g':'', '8h':''}
     #checks if there's at least a king or queen in board
-    if 'bking' in board.values() == False:
-        return False
-    if 'wking' in board.values() == False:
-        return False
+    if 'bking'  not in board.values():
+        return print('Error! No black king on the chessboard')
+    if 'wking' not in board.values():
+        return print('Error! No white king on the chessboard')
     # if board key is not in valid spaces, returns false
     for key in board.keys():
         if key not in validspaces.keys():
@@ -42,6 +42,6 @@ def isValidChessBoard(board):
     print(totalpieces)
 
 
-testboard = {'1h': 'bking', '6c': 'wqueen', '2g': 'bbishop', '5h': 'bqueen', '3e': 'wking'}
+testboard = { '6c': 'wqueen', '2g': 'bbishop', '5h': 'bqueen', '3e': 'wking'}
 isValidChessBoard(testboard)
 
